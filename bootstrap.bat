c8f33620031ecddaf158e01d1280e84a12c71097
Jakub Neruda <nerudaj@email.cz>
2021-10-19 18:55:07 +0200

    Create bootstrap.bat

---
+++ bootstrap.bat
@@ -0,0 +1,7 @@
+@echo off
+
+copy "cmake\cpp.cmake" "examples\cmake"
+copy "cmake\utils.cmake" "examples\cmake"
+
+copy "cmake\cpp.cmake" "project-template\cmake"
+copy "cmake\utils.cmake" "project-template\cmake"
